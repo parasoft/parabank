6355c279f293ca926f43193b737e24aee196d214
Mark Lambert <mlambert@parasoft.com>
2016-06-29 21:07:33 -0700

    Adding Parasoft SOAtest Functional Tests

--- jtest-sa-ut.bat
+++ jtest-sa-ut.bat
@@ -1,4 +1,3 @@
-REM bat script for App Coverage Demo (Jtest)
 @echo off
 call set-vars
 
@@ -7,17 +6,16 @@ echo Running Static Analysis and Unit Tests against build.id=%BUILD_ID%
 echo ===================================================================
 
 echo **1/3** Execute Static Analysis: Recommended Rules
-call mvn jtest:jtest -Djtest.config="jtest.dtp://Recommended Rules" -Dmaven.test.skip=true -Dmaven.test.failure.ignore=true -Dproperty.dtp.project="Parabank v3" -Djtest.showsettings=true -Djtest.report=jtest-sa -Dproperty.console.verbosity.level=high -Dproperty.build.id=%BUILD_ID% -Djacoco.skip=true > jtest-sa-%RUN_TIME%.log 2>&1
+call mvn jtest:jtest -Djtest.config="jtest.dtp://Recommended Rules" -Dmaven.test.skip=true -Dmaven.test.failure.ignore=true -Dproperty.dtp.project=%DTP_PROJECT% -Djtest.showsettings=true -Djtest.report=jtest-sa -Dproperty.console.verbosity.level=high -Dproperty.build.id=%BUILD_ID% -Djacoco.skip=true > jtest-sa-%RUN_TIME%.log 2>&1
 
 rem echo **X/X** (Optional) Execute Static Analysis: All Rules
-rem call mvn jtest:jtest -Djtest.config="jtest.dtp://All Rules" -Dmaven.test.skip=true -Dmaven.test.failure.ignore=true -Dproperty.dtp.project=Parabank-3 -Djtest.showsettings=true -Djtest.report=jtest-sa-all -Dproperty.console.verbosity.level=high -Dproperty.build.id=%BUILD_ID% -Djacoco.skip=true > jtest-sa-all-%RUN_TIME%.log 2>&1
+rem call mvn jtest:jtest -Djtest.config="jtest.dtp://All Rules" -Dmaven.test.skip=true -Dmaven.test.failure.ignore=true -Dproperty.dtp.project=%DTP_PROJECT% -Djtest.showsettings=true -Djtest.report=jtest-sa-all -Dproperty.console.verbosity.level=high -Dproperty.build.id=%BUILD_ID% -Djacoco.skip=true > jtest-sa-all-%RUN_TIME%.log 2>&1
 
 echo **2/3** Execute Metrics
-call mvn jtest:jtest -Djtest.config="jtest.dtp://Metrics" -Dmaven.test.skip=true -Dmaven.test.failure.ignore=true -Dproperty.dtp.project="Parabank v3" -Djtest.showsettings=true -Djtest.report=jtest-ma -Dproperty.console.verbosity.level=high -Dproperty.build.id=%BUILD_ID% -Djacoco.skip=true > jtest-ma-%RUN_TIME%.log 2>&1
+call mvn jtest:jtest -Djtest.config="jtest.dtp://Metrics" -Dmaven.test.skip=true -Dmaven.test.failure.ignore=true -Dproperty.dtp.project=%DTP_PROJECT% -Djtest.showsettings=true -Djtest.report=jtest-ma -Dproperty.console.verbosity.level=high -Dproperty.build.id=%BUILD_ID% -Djacoco.skip=true > jtest-ma-%RUN_TIME%.log 2>&1
 
 echo **3/3** Execute Unit Tests
-call mvn process-test-classes jtest:agent test jtest:jtest -Djtest.config="builtin://Unit Tests" -Dmaven.test.failure.ignore=true -Dproperty.dtp.project="Parabank v3" -Dproperty.build.id=%BUILD_ID% -Dproperty.report.coverage.images="Parabank-UT;Parabank-All" -Dproperty.console.verbosity.level=high -Djtest.showsettings=true -Djtest.report=jtest-ut -Djacoco.skip=true > jtest-ut-%RUN_TIME%.log 2>&1
-
+call mvn process-test-classes jtest:agent test jtest:jtest -Djtest.config="builtin://Unit Tests" -Dmaven.test.failure.ignore=true -Dproperty.dtp.project=%DTP_PROJECT% -Dproperty.build.id=%BUILD_ID% -Dproperty.report.coverage.images="Parabank-UT;Parabank-All" -Dproperty.console.verbosity.level=high -Djtest.showsettings=true -Djtest.report=jtest-ut -Djacoco.skip=true > jtest-ut-%RUN_TIME%.log 2>&1
 
 echo =================================================================
 echo Finished Static Analysis and Unit Testing of build.id=%BUILD_ID%
--- set-vars.bat
+++ set-vars.bat
@@ -1,5 +1,12 @@
 @echo off
 rem **** Setup
+set DTP_PROJECT=Parabank-v3
 set BUILD_ID=PARABANK3-%date:~10,4%%date:~4,2%%date:~7,2%-%BUILD_NUMBER%
 set RUN_TIME=%date:~10,4%%date:~4,2%%date:~7,2%-%time:~0,2%%time:~3,2%
+
+set PARABANK-TOMCAT=C:\tomcat-8.0.26-parabank
+
+echo ===================================================================
+echo DTP Project: %DTP_PROJECT%
+echo ===================================================================
 
