9e8fe85cc518905e3b984d8253bbadf84cfca883
Nick Rapoport <nrapopor@hotmail.com>
2017-01-27 19:33:24 -0500

    Fixed minor bug in the set-vars.bat. The fix aligned the windows script to the Linux one, as much as practical

--- set-vars.bat
+++ set-vars.bat
@@ -4,12 +4,10 @@ set DTP_PROJECT=Parabank-v3
 
 IF "%BUILD_NUMBER%"=="" (
 	set BUILD_ID=PARABANK3-%date:~10,4%%date:~4,2%%date:~7,2%
-	Goto buildIdSet:
+) else (
+	set BUILD_ID=PARABANK3-%BUILD_NUMBER%
 )
 
-set BUILD_ID=PARABANK3-%BUILD_NUMBER%
-
-:buildIdSet
 set RUN_TIME=%date:~10,4%%date:~4,2%%date:~7,2%-%time:~1,1%%time:~3,2%
 
 set APP_COVERAGE_DIR=c:\tmp
