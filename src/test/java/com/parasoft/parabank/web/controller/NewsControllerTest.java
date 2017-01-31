52b5625ce9e00b2f3a0f1313fe8ce4d1c4ffffff
Joseph Benken <benken@parasoft.com>
2017-01-31 13:20:26 -0800

    NewsControllerTest - fix failure when current date is January 31st

--- src/test/java/com/parasoft/parabank/web/controller/NewsControllerTest.java
+++ src/test/java/com/parasoft/parabank/web/controller/NewsControllerTest.java
@@ -1,6 +1,7 @@
 package com.parasoft.parabank.web.controller;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.*;
+import static org.hamcrest.CoreMatchers.*;
 
 import java.util.Date;
 import java.util.List;
@@ -39,6 +40,11 @@ public class NewsControllerTest extends AbstractControllerTest<NewsController> {
         assertEquals(Constants.NEWS, mav.getViewName());
 
         final Map<Date, List<News>> news = (Map<Date, List<News>>) getModelValue(mav, Constants.NEWS);
-        assertEquals(4, news.size());
+        assertThat(news.size(), anyOf(equalTo(3), equalTo(4)));  // 3 dates if January 31st
+        int numNews = 0;
+        for (Map.Entry<Date, List<News>> entry : news.entrySet()) {
+            numNews += entry.getValue().size();
+        }
+        assertEquals(6, numNews);
     }
 }
