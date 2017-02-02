621dbd79fb721b16439396366c06d27f35ecf75f
Joseph Benken <benken@parasoft.com>
2017-02-02 14:31:57 -0800

    fix CustomWadlGenerator removing Swagger ApiListingResource
    The swagger.json/yaml would no longer be accessible after viewing
    the WADL.

--- src/main/java/com/parasoft/parabank/util/CustomWadlGenerator.java
+++ src/main/java/com/parasoft/parabank/util/CustomWadlGenerator.java
@@ -1,6 +1,6 @@
 package com.parasoft.parabank.util;
 
-import java.util.Iterator;
+import java.util.ArrayList;
 import java.util.List;
 
 import javax.ws.rs.core.UriInfo;
@@ -12,16 +12,19 @@ import org.apache.cxf.message.Message;
 import io.swagger.jaxrs.listing.ApiListingResource;
 
 public class CustomWadlGenerator extends WadlGenerator {
+
     @Override
     public List<ClassResourceInfo> getResourcesList(Message m, UriInfo ui) {
-        List<ClassResourceInfo> cris = super.getResourcesList(m, ui);
-        Iterator<ClassResourceInfo> it = cris.iterator();
-        while (it.hasNext()) {
+        List<ClassResourceInfo> resources = super.getResourcesList(m, ui);
+        // Must return a copy.  Otherwise, removing ApiListingResource from the
+        // original list would remove the resource from JAX-RS entirely.
+        List<ClassResourceInfo> copy = new ArrayList<>(resources.size());
+        for (ClassResourceInfo resource : resources) {
             // Filter out ApiListingResource, which does not make sense to include in our WADL.
-            if (ApiListingResource.class.isAssignableFrom(it.next().getResourceClass())) {
-                it.remove();
+            if (!ApiListingResource.class.isAssignableFrom(resource.getResourceClass())) {
+                copy.add(resource);
             }
         }
-        return cris;
+        return copy;
     }
 }
