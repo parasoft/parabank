762700d2ec44e8c75ae2a321ffee4cdf8b0272d5
Sang U. Seong <sang@parasoft.com>
2017-09-27 10:03:03 -0700

    clear coverage data before generating static coverage

--- deploy-jtest-monitor.bat
+++ deploy-jtest-monitor.bat
@@ -10,7 +10,9 @@ echo Deploying Jtest Application Coverage Monitor for build.id=%BUILD_ID%
 echo ===================================================================
 
 echo **1/3** Generate montior package
-call mvn package jtest:monitor -Dmaven.test.failure.ignore=true > %LOG_DIR%\jtest-monitor-%RUN_TIME%.log
+call erase /Q target\jtest\runtime_coverage\__default__\runtime_coverage*
+
+call mvn package jtest:monitor -Dmaven.test.skip=true -Dmaven.test.failure.ignore=true > %LOG_DIR%\jtest-monitor-%RUN_TIME%.log
 
 echo **2/3** Cleaning up old coverage data
 call erase /Q /S %APP_COVERAGE_DIR%\monitor\runtime_coverage\*
