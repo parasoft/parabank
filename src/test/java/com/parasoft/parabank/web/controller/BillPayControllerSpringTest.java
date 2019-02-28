b637b7f732243089e67a39a1d6b2cbe53ff58ea1
Tony Yoshicedo <tyoshicedo@parasoft.com>
2019-02-28 08:26:47 -1000

    @task SOA-11289 - Add email field to bill pay, move phone number for selenium agent demo
    Reviewers: grigorb
    
    Reviewed By: grigorb
    
    JIRA Issues: SOA-11289
    
    Differential Revision: http://phabricator.parasoft.com/D3280

--- src/test/java/com/parasoft/parabank/web/controller/BillPayControllerSpringTest.java
+++ src/test/java/com/parasoft/parabank/web/controller/BillPayControllerSpringTest.java
@@ -65,13 +65,16 @@ public class BillPayControllerSpringTest {
         final BillPayForm form = new BillPayForm();
         final Payee payee = new Payee();
         final Address address = new Address();
+        final ContactInformation contactInformation = new ContactInformation();
         address.setStreet("payee street");
         address.setCity("payee city");
         address.setState("payee state");
         address.setZipCode("payee zipcode");
         payee.setName("payee name");
         payee.setAddress(address);
-        payee.setPhoneNumber("payee phone number");
+        contactInformation.setPhoneNumber("payee phone number");
+        contactInformation.setEmail("payee@email.com");
+        payee.setContactInformation(contactInformation);
         payee.setAccountNumber(100);
         form.setPayee(payee);
         form.setVerifyAccount(100);
@@ -147,13 +150,13 @@ public class BillPayControllerSpringTest {
     public void testValidate() throws Exception
     {
         BillPayForm form = getBillPayForm();
-        form.getPayee().setPhoneNumber(null);
+        form.getPayee().getContactInformation().setPhoneNumber(null);
         mockMvc.perform(post(PATH)
             .contentType(MediaType.APPLICATION_FORM_URLENCODED)
             .sessionAttr(Constants.BILLPAYFORM, form)
             .sessionAttr(Constants.USERSESSION, new UserSession(new Customer())))
                 .andExpect(model().hasErrors())
-                .andExpect(model().attributeHasFieldErrorCode(Constants.BILLPAYFORM, "payee.phoneNumber", "error.phone.number.required"));
+                .andExpect(model().attributeHasFieldErrorCode(Constants.BILLPAYFORM, "payee.contactInformation.phoneNumber", "error.phone.number.required"));
 
         form = getBillPayForm();
         form.setAmount(null);
