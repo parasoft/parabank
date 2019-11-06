b4b7ea95e3cedac7ebdd6090d82e2c1091d114a7
Joseph Benken <benken@parasoft.com>
2019-11-05 17:44:12 -0800

    WebServiceLoanProvider - fix building wrong WSDL URL in some cases

--- src/main/java/com/parasoft/parabank/messaging/WebServiceLoanProvider.java
+++ src/main/java/com/parasoft/parabank/messaging/WebServiceLoanProvider.java
@@ -1,20 +1,30 @@
 package com.parasoft.parabank.messaging;
 
-import java.lang.reflect.*;
-import java.net.*;
-import java.util.*;
+import java.lang.reflect.Method;
+import java.net.MalformedURLException;
+import java.net.URI;
+import java.net.URL;
+import java.util.ArrayList;
 
-import javax.annotation.*;
-import javax.management.*;
-import javax.xml.namespace.*;
-import javax.xml.ws.*;
+import javax.annotation.Resource;
+import javax.management.MBeanServer;
+import javax.management.MBeanServerFactory;
+import javax.management.ObjectName;
+import javax.xml.namespace.QName;
+import javax.xml.ws.BindingProvider;
+import javax.xml.ws.Service;
 
-import org.slf4j.*;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
-import com.parasoft.parabank.domain.*;
-import com.parasoft.parabank.domain.logic.*;
-import com.parasoft.parabank.service.*;
-import com.parasoft.parabank.util.*;
+import com.parasoft.parabank.domain.LoanRequest;
+import com.parasoft.parabank.domain.LoanResponse;
+import com.parasoft.parabank.domain.logic.AdminManager;
+import com.parasoft.parabank.domain.logic.AdminParameters;
+import com.parasoft.parabank.domain.logic.LoanProvider;
+import com.parasoft.parabank.service.LoanProcessorService;
+import com.parasoft.parabank.service.ParaBankServiceException;
+import com.parasoft.parabank.util.Util;
 
 /**
  * Message client for generating and sending loan requests over SOAP
@@ -22,35 +32,44 @@ import com.parasoft.parabank.util.*;
 public class WebServiceLoanProvider implements LoanProvider {
     private static final Logger log = LoggerFactory.getLogger(WebServiceLoanProvider.class);
 
-    private static final String NL_LOCALHOST = "localhost";
+    private static final String NL_LOCALHOST = "localhost"; //$NON-NLS-1$
 
     /**
-     * Retrieve the HTTP port currently in use by Tomcat. Returns the default port if any errors occur during retrieval.
+     * Retrieve the URI currently in use by Tomcat. Returns null if any errors occur during retrieval.
      */
-    private static int getPort() {
+    private static URI getLocalUri(String path, String query) {
+        URI ret = null;
         try {
             final ArrayList<?> mBeanServers = MBeanServerFactory.findMBeanServer(null);
             if (mBeanServers != null && mBeanServers.size() > 0 && mBeanServers.get(0) instanceof MBeanServer) {
                 final MBeanServer mBeanServer = (MBeanServer) mBeanServers.get(0);
-                final ObjectName name = new ObjectName("Catalina", "type", "Server"); // parasoft-suppress CUSTOM.SLR  "Class name"
-                final Object server = mBeanServer.getAttribute(name, "managedResource"); // parasoft-suppress CUSTOM.SLR "Class name"
+                final ObjectName name = new ObjectName("Catalina", "type", "Server"); //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$
+                final Object server = mBeanServer.getAttribute(name, "managedResource"); //$NON-NLS-1$
                 if (server != null) {
-                    final Method findServices = server.getClass().getMethod("findServices"); // parasoft-suppress CUSTOM.SLR "Method name"
+                    final Method findServices = server.getClass().getMethod("findServices"); //$NON-NLS-1$
                     final Object[] services = (Object[]) findServices.invoke(server);
                     for (final Object service : services) {
-                        final Method findConnectors = service.getClass().getMethod("findConnectors"); // parasoft-suppress CUSTOM.SLR "Method name"
+                        final Method findConnectors = service.getClass().getMethod("findConnectors"); //$NON-NLS-1$
                         final Object[] connectors = (Object[]) findConnectors.invoke(service);
                         for (final Object connector : connectors) {
-                            final Method getProtocolHandler = connector.getClass().getMethod("getProtocolHandler"); // parasoft-suppress CUSTOM.SLR "Method name"
+                            final Method getProtocolHandler = connector.getClass().getMethod("getProtocolHandler"); //$NON-NLS-1$
                             final Object protocolHandler = getProtocolHandler.invoke(connector);
                             final String handlerType = protocolHandler.getClass().getName();
-                            if (handlerType.endsWith("Http11Protocol") // parasoft-suppress CUSTOM.SLR "Class name"
-                                || handlerType.endsWith("Http11AprProtocol") // parasoft-suppress CUSTOM.SLR "Class name"
-                                || handlerType.endsWith("Http11NioProtocol")) { // parasoft-suppress CUSTOM.SLR "Class name"
-                                final Method getPort = connector.getClass().getMethod("getPort"); // parasoft-suppress CUSTOM.SLR "Method name"
+                            if (handlerType.endsWith("Http11Protocol") //$NON-NLS-1$
+                                || handlerType.endsWith("Http11AprProtocol") //$NON-NLS-1$
+                                || handlerType.endsWith("Http11NioProtocol")) { //$NON-NLS-1$
+                                final Method getPort = connector.getClass().getMethod("getPort"); //$NON-NLS-1$
                                 final Object port = getPort.invoke(connector);
                                 if (port instanceof Integer) {
-                                    return (Integer) port;
+                                    final Method getScheme = connector.getClass().getMethod("getScheme"); //$NON-NLS-1$
+                                    Object schemeObj = getScheme.invoke(connector);
+                                    if (schemeObj instanceof String) {
+                                        String scheme = (String) schemeObj;
+                                        // favor http over https
+                                        if (ret == null || "http".equalsIgnoreCase(scheme)) { //$NON-NLS-1$
+                                            ret = new URI(scheme, null, NL_LOCALHOST, (Integer) port, path, query, null);
+                                        }
+                                    }
                                 }
                             }
                         }
@@ -61,7 +80,7 @@ public class WebServiceLoanProvider implements LoanProvider {
             // We're not in Tomcat or something unexpected happened, so we
             // assume the default port
         }
-        return -1;
+        return ret;
     }
 
     @Resource(name = "adminManager")
@@ -74,16 +93,14 @@ public class WebServiceLoanProvider implements LoanProvider {
         try {
             // If wsdlUrl is localhost, replace the port in the WSDL with the
             // runtime HTTP port
-            final String[] wsdlUrlSplit = wsdlUrl.split("/");
-            if (wsdlUrlSplit.length >= 3 && !Util.isEmpty(wsdlUrlSplit[2])
-                && wsdlUrlSplit[2].startsWith(NL_LOCALHOST)) {
-                final int currentPort = getPort();
-                if (currentPort > 0) {
-                    wsdlUrl = wsdlUrl.replaceFirst(wsdlUrlSplit[2], NL_LOCALHOST + ":" + currentPort);
+            URL wsdlURL = new URL(wsdlUrl);
+            if (NL_LOCALHOST.equalsIgnoreCase(wsdlURL.getHost())) {
+                URI localUri = getLocalUri(wsdlURL.getPath(), wsdlURL.getQuery());
+                if (localUri != null) {
+                    wsdlURL = localUri.toURL();
                 }
             }
-            final URL wsdlURL = new URL(wsdlUrl);
-            final QName serviceName = new QName(LoanProcessorService.TNS, "LoanProcessorServiceImplService"); // parasoft-suppress CUSTOM.SLR "Class name"
+            final QName serviceName = new QName(LoanProcessorService.TNS, "LoanProcessorServiceImplService"); //$NON-NLS-1$
             final Service service = Service.create(wsdlURL, serviceName);
             final LoanProcessorService client = service.getPort(LoanProcessorService.class);
 
@@ -94,9 +111,9 @@ public class WebServiceLoanProvider implements LoanProvider {
 
             return client.requestLoan(loanRequest);
         } catch (final MalformedURLException e) {
-            log.error("{} caught :", e.getClass().getSimpleName(), e);
+            log.error("{} caught :", e.getClass().getSimpleName(), e); //$NON-NLS-1$
         } catch (final ParaBankServiceException e) {
-            log.error("{} caught :", e.getClass().getSimpleName(), e);
+            log.error("{} caught :", e.getClass().getSimpleName(), e); //$NON-NLS-1$
         }
         return null;
     }
