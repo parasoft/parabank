7460c0139a75c9466da525149284d424ee62a257
Brian McGlauflin <bmcglau@parasoft.com>
2023-04-10 14:36:55 -0700

    Fix JUnit by skipping ContactInformation.equals() validation
    Reviewers: benken
    
    Reviewed By: benken
    
    Differential Revision: http://phabricator.parasoft.com/D6322

--- src/test/java/com/parasoft/parabank/domain/PayeeTest.java
+++ src/test/java/com/parasoft/parabank/domain/PayeeTest.java
@@ -1,5 +1,28 @@
 package com.parasoft.parabank.domain;
 
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
 import com.parasoft.parabank.test.util.AbstractBeanTestCase;
 
-public class PayeeTest extends AbstractBeanTestCase<Payee> { }
+public class PayeeTest extends AbstractBeanTestCase<Payee> {
+
+    @Test
+    @Override
+    public void testEqualsContractMet() {
+        // Do not use the normal test for equals(), since ContactInformation has a known (and desired) bug in equals()
+        Object o1;
+        Object o2;
+        try {
+            o1 = beanClass.newInstance();
+            o2 = beanClass.newInstance();
+
+            assertTrue("Instances with default constructor not equal (o1.equals(o2))", o1.equals(o2));
+            assertTrue("Instances with default constructor not equal (o2.equals(o1))", o2.equals(o1));
+        } catch (final InstantiationException | IllegalAccessException e) {
+            e.printStackTrace();
+            throw new AssertionError("Unable to construct an instance of the class under test:" + beanClass.getName());
+        }
+    }
+}
